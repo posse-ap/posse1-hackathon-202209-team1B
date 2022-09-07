5105f780a87ebc40dbaaf60658e50f16a1fd7faa
【daiki】 <daiki20010705@icloud.com>
2022-09-08 03:06:01 +0900

    Add : カテゴリ名,備品名表示

--- web/resources/views/items_list/index.blade.php
+++ web/resources/views/items_list/index.blade.php
@@ -1,3 +1,10 @@
 <x-guest-layout>
     <p>{{ $items_amount }}件見つかりました</p>
+
+    @foreach ($items as $item)
+        <div>
+            <p class="font-bold">{{ $item->category->name }}</p>
+            <p class="font-bold">{{ $item->name }}</p>
+        </div>
+    @endforeach
 </x-guest-layout>
