ebf0b9d72c7b2433cc3140f15e4e64f99bd71e69
ryudai <bigdragon.06102001@gmail.com>
2022-09-08 23:54:50 +0900

    見た目の修正

--- web/resources/views/items_list/index.blade.php
+++ web/resources/views/items_list/index.blade.php
@@ -1,40 +1,40 @@
 <x-guest-layout>
-    <section>
-        <h2>「」の備品一覧</h2>
-        <form method="GET" action="{{ route('items_list.search') }}" class="flex items-center shadow">
-            <p>カテゴリ：</p>
+    <section class="w-[1280px] mx-auto mt-10">
+        <h2 class="text-2xl font-bold border-l-4 py-2 pl-2 border-[#0071BC]">「」の備品一覧</h2>
+        <form method="GET" action="{{ route('items_list.search') }}" class="flex items-center mt-10">
+            <p class="w-20 text-right font-bold text-black opacity-50 mr-4">カテゴリ：</p>
             <div class="container flex mx-auto">
-                <div class="flex relative">
-                    <button class="PButton-primary w-full" type="submit" value="全て" name="category_id">全て</button>
+                <div class="flex relative gap-3">
+                    <button class="PButton-primary px-3 py-1" type="submit" value="" name="category_id">全て</button>
                     @foreach ($categories as $category)
-                        <button class="PButton-primary w-full" type="submit" value="{{ $category->id }}"
+                        <button class="PButton-primary px-3 py-1" type="submit" value="{{ $category->id }}"
                             name="category_id">{{ $category->name }}</button>
                     @endforeach
                 </div>
             </div>
         </form>
-        <form method="GET" action="{{ route('items_list.search') }}" class="flex items-center shadow">
-            <p>状態：</p>
+        <form method="GET" action="{{ route('items_list.search') }}" class="flex items-center mt-5">
+            <p class="w-20 text-right font-bold text-black opacity-50 mr-4">状態：</p>
             <div class="container flex mx-auto">
-                <div class="flex relative">
-                        <button class="PButton-primary w-full" type="submit" value="全て" name="status">全て</button>
-                        <button class="PButton-primary w-full" type="submit" value="利用可能" name="status">利用可能</button>
-                        <button class="PButton-primary w-full" type="submit" value="利用中" name="status">利用中</button>
+                <div class="flex relative gap-3">
+                    <button class="PButton-primary px-3 py-1" type="submit" value="" name="status">全て</button>
+                    <button class="PButton-primary px-3 py-1" type="submit" value="利用可能" name="status">利用可能</button>
+                    <button class="PButton-primary px-3 py-1" type="submit" value="利用中" name="status">利用中</button>
                 </div>
             </div>
         </form>
-        <form method="GET" action="{{ route('items_list.search') }}" class="flex items-center shadow">
-            <p>並び順：</p>
+        <form method="GET" action="{{ route('items_list.search') }}" class="flex items-center mt-5">
+            <p class="w-20 text-right font-bold text-black opacity-50 mr-4">並び順：</p>
             <div class="container flex mx-auto">
-                <div class="flex relative">
-                    <button type="submit" value="新着順" name="sort">新着順</button>
-                    <button type="submit" value="人気順" name="sort">人気順</button>
+                <div class="flex relative gap-3">
+                    <button class="PButton-primary px-3 py-1" type="submit" value="新着順" name="sort">新着順</button>
+                    <button class="PButton-primary px-3 py-1" type="submit" value="人気順" name="sort">人気順</button>
                 </div>
             </div>
         </form>
     </section>
 
-    <section class="w-[1240px] mx-auto mt-10">
+    <section class="w-[1280px] mx-auto mt-10">
         <p><span class="font-bold text-lg mx-2">{{ $items_amount }}</span>件見つかりました</p>
 
         <div class="grid grid-cols-4 gap-y-10 mt-10">
