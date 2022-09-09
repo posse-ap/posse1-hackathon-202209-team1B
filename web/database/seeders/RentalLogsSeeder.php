3079dfd395e20f1b4fbe70b7cabd786fbb3cd037
【daiki】 <daiki20010705@icloud.com>
2022-09-09 12:37:51 +0900

    Add : RentalLogsSeeder

--- web/database/seeders/RentalLogsSeeder.php
+++ web/database/seeders/RentalLogsSeeder.php
@@ -21,7 +21,28 @@ class RentalLogsSeeder extends Seeder
                 'item_id' => 1,
                 'start_date' => "2022-08-08 00:00:00",
                 'end_date' => "2022-08-09 00:00:00",
+                'return_date' => null,
+            ],
+            [
+                'user_id' => 1,
+                'item_id' => 3,
+                'start_date' => "2022-08-08 00:00:00",
+                'end_date' => "2022-08-09 00:00:00",
                 'return_date' => "2022-09-09 00:00:00",
+            ],
+            [
+                'user_id' => 1,
+                'item_id' => 4,
+                'start_date' => "2022-08-08 00:00:00",
+                'end_date' => "2022-08-09 00:00:00",
+                'return_date' => "2022-09-09 00:00:00",
+            ],
+            [
+                'user_id' => 1,
+                'item_id' => 5,
+                'start_date' => "2022-08-08 00:00:00",
+                'end_date' => "2022-08-09 00:00:00",
+                'return_date' => null,
             ],
             [
                 'user_id' => 2,
@@ -35,7 +56,7 @@ class RentalLogsSeeder extends Seeder
                 'item_id' => 1,
                 'start_date' => "2022-08-08 00:00:00",
                 'end_date' => "2022-08-09 00:00:00",
-                'return_date' => "2022-09-09 00:00:00",
+                'return_date' => null,
             ],
             [
                 'user_id' => 2,
@@ -46,6 +67,20 @@ class RentalLogsSeeder extends Seeder
             ],
             [
                 'user_id' => 3,
+                'item_id' => 2,
+                'start_date' => "2022-08-10 00:00:00",
+                'end_date' => "2022-08-11 00:00:00",
+                'return_date' => "2022-09-09",
+            ],
+            [
+                'user_id' => 6,
+                'item_id' => 2,
+                'start_date' => "2022-08-10 00:00:00",
+                'end_date' => "2022-08-11 00:00:00",
+                'return_date' => "2022-09-09",
+            ],
+            [
+                'user_id' => 7,
                 'item_id' => 2,
                 'start_date' => "2022-08-10 00:00:00",
                 'end_date' => "2022-08-11 00:00:00",
